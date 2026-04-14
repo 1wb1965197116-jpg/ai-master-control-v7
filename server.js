004c867444c2c9a157d8a0d017e120cd5ba058f9
1wbillingsley-git <1wb1965197116@gmail.com>
2026-04-14 02:28:07 -0400

    Update server to run company metrics and version

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const { buildSystem } = require("./core/factory");
+const { runCompany } = require("./core/company");
+const { getMetrics } = require("./brain/metrics");
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("🧠 AI MASTER CONTROL v9 ONLINE");
+  res.send("🏢 AI AUTONOMOUS COMPANY OS v12 ONLINE");
 });
 
-app.get("/build", async (req, res) => {
-  const goal = req.query.goal || "basic app";
+app.get("/run", async (req, res) => {
+  const metrics = getMetrics();
 
-  const result = await buildSystem(goal);
+  const result = await runCompany(metrics);
 
   res.json(result);
 });
 
 app.listen(5000, () => {
-  console.log("🚀 v9 running on port 5000");
+  console.log("🚀 v12 Company OS running");
 });
